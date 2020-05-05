d8d93f729a6354a76de646e662715f79654c8764
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-05 00:31:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,6 @@
-
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-
-#introduction_with_language
 
 def introduction(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #(language)."
